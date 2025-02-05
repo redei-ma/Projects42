6c6dc26803429d3ac153740ddaeb8f5073924fa7
redei-ma <renato.deimarini@gmail.com>
2025-02-05 20:01:17 +0100

    mod

--- CommonCore/Milestone2/push_swap/libft/libft.h
+++ CommonCore/Milestone2/push_swap/libft/libft.h
@@ -20,8 +20,6 @@
 # include <ctype.h>
 # include <limits.h>
 # include <stdint.h>
-# include "get_next_line_bonus.h"
-# include "ft_printf.h"
 
 typedef struct s_list
 {
@@ -53,6 +51,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+int		ft_printf(const char *format, ...);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
